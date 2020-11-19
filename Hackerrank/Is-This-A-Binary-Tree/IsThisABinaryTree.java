ba15cfbae626b75d682ba64bdd8836616c08e096
Shahariar Hossen <shahariar.hossen@datsoft-bd.com>
2020-11-19 09:06:10 +0600

    tree traversal

--- Hackerrank/Is-This-A-Binary-Tree/IsThisABinaryTree.java
+++ Hackerrank/Is-This-A-Binary-Tree/IsThisABinaryTree.java
@@ -4,15 +4,15 @@ public class IsThisABinaryTree {
 
     }
 
-    static boolean checkBST(Node root) {
-        return isBST(root, -1, -1);
-    }
+        static boolean checkBST(Node root) {
+            return isBST(root, 0, 1000);
+        }
 
-    static boolean isBST(Node root, int leftChild, int rightChild){
-        if(root == null) return true;
-        else if(leftChild >= 0 && root.data <= leftChild) return false;
-        else if(rightChild >= 0 && root.data >= rightChild) return false;
-        // check both left subtree and right subtree
-        return (isBST(root.left, leftChild, root.data)) && (isBST(root.right,root.data,rightChild));
-    }
+        static boolean isBST(Node root, int leftChild, int rightChild){
+            if(root == null) return true;
+            else if(leftChild >= 0 && root.data <= leftChild) return false;
+            else if(rightChild >= 0 && root.data >= rightChild) return false;
+            // check both left subtree and right subtree
+            return (isBST(root.left, leftChild, root.data)) && (isBST(root.right,root.data,rightChild));
+        }
 }
---
+++ SPOJ/TreeVerse - Traversing Tree/Java/TreeVerse.java
@@ -0,0 +1,26 @@
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item){
+        key = item;
+        left = right = null;
+    }
+}
+
+public class TreeVerse {
+    Node root;
+    TreeVerse(){
+        root = null;
+    }
+    public static void main(String[] args) {
+        
+    }
+
+    // Print Pre-Order
+    protected static void printPreOrder(Node node){
+        if(node == null) return;
+    }
+    // Print In-Order
+    // Print Post-Order
+}
